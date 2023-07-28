76f26af63fb5a6e36a13da1c2ed66c8de9776866
EskurinhoES <eskurinhoes082023@gmail.com>
2023-07-28 02:13:25 -0300

    ambiente virtual criado com sucesso

---
+++ exercicios/exercicio002.py
@@ -0,0 +1,9 @@
+"""
+Programa que pede ao usuário três números inteiros e mostra a soma deles
+"""
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+num3 = int(input('Digite o segundo valor: '))
+soma = num1 + num2 + num3
+
+print(f'A soma entre {num1}, {num2} e {num3} é : {soma} .')
